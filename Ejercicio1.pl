97006e149f26ab6961ad420c5ea3804d4a40d024
Gustavo <gordono@unsa.edu.pe>
2021-11-30 03:00:20 +0000

    Se ha realizado la conexión con el SGBD y se ha realizado la consulta que nos pide la consigna, lo que nos devolvió un fetchrow_array y lo mandamos al STDERR

---
+++ Ejercicio1.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use DBI;
+
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.11";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+#Consultas al SGBD
+my $id = 5;
+
+my $sth = $dbh->prepare("SELECT Name FROM Actor WHERE ActorId=?");
+$sth->execute($id);
+
+my $resp;
+if(my @row = $sth->fetchrow_array) {
+  print STDERR "@row\n";
+} else {
+  print "No hay un Actor con el ID $id!\n"; #Vacío!
+}
+
+$sth->finish;
+$dbh->disconnect;
+
