63655a067a58652072e54aa3715d2645d1896d2a
Gustavo <gordono@unsa.edu.pe>
2021-11-30 04:37:31 +0000

    Ejercicio4 (Versión Extendida): Previamente planteada la lógica, se ha comenzado a construir los arreglos para almacenar los diversos campos que vamos a presentar después de la consulta

--- queryByYear.pl
+++ queryByYear.pl
@@ -14,23 +14,37 @@ my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 #Consultas al SGBD
 my $year = $q->param('year');
 
-my $sth = $dbh->prepare("SELECT Title FROM Movie WHERE Year=?");
+my $sth = $dbh->prepare("SELECT Title, Year, Score, Votes FROM Movie WHERE Year=?");
 $sth->execute($year);
 
-my @resp;
+my @titles;
+my @years;
+my @scores;
+my @votes;
 while(my @row = $sth->fetchrow_array) {
+  print STDERR "registro: @row\n";
   my $title = $row[0];
-  push(@resp, $title);
+  my $anio = $row[1];
+  my $score = $row[2];
+  my $votes = $row[3];
+  push(@titles, $title);
+  push(@years, $anio);
+  push(@scores, $score);
+  push(@votes, $votes);
 }
+print STDERR "Titulos: @titles\n";
+print STDERR "Años:  @years\n";
+print STDERR "Puntajes: @scores\n";
+print STDERR "Votos: @votes\n";
 
 $sth->finish;
 $dbh->disconnect;
 
 print $q->header('text/html;charset=UTF-8');
 
-my $body = renderTable($year, @resp);
+#my $body = renderTable($year, @resp);
 
-print renderHTMLpage("Películas de $year", 'css/mystyle.css', $body);
+#print renderHTMLpage("Películas de $year", 'css/mystyle.css', $body);
 
 sub renderHTMLpage {
   my $title = $_[0];
