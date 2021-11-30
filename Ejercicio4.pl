452b19ebcd0e96fea087b5dc999bb029e5bf53c3
Gustavo <gordono@unsa.edu.pe>
2021-11-30 03:43:54 +0000

    Ejercicio4: Se ha mandado al STDERR el título del Documento

--- Ejercicio4.pl
+++ Ejercicio4.pl
@@ -5,35 +5,13 @@ use CGI;
 use DBI;
 
 my $q = CGI->new;
-
-my $user = 'alumno';
-my $password = 'pweb1';
-my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.11";
-my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
-
-#Consultas al SGBD
-my $score = 7;
-my $votes = 5000;
-
-my $sth = $dbh->prepare("SELECT Title FROM Movie WHERE Score>? AND Votes>?");
-$sth->execute($score, $votes);
-
-my @resp;
-while(my @row = $sth->fetchrow_array) {
-  print STDERR "registro: @row\n";
-  my $title = $row[0];
-  push(@resp, $title);
-}
-
-$sth->finish;
-$dbh->disconnect;
-
 print $q->header('text/html;charset=UTF-8');
 
-my $body = renderTable(@resp);
-print STDERR "@resp\n";
+my $titulo = "Perl y MariaDB: Consulta de Películas";
+print STDERR "$titulo\n";
+#my $body = renderBody($titulo);
 
-print renderHTMLpage('Películas de 1985', 'css/mystyle.css', $body);
+#print renderHTMLpage('Consulta de Películas', 'css/mystyle.css', $body);
 
 sub renderHTMLpage {
   my $title = $_[0];
@@ -56,12 +34,3 @@ HTML
   return $html;
 }
 
-sub renderTable {
-  my @table  = @_;
-
-  my $body = "<h1 class='titulo'>Películas de 1985</h1>\n<table>\n";
-  foreach my $reg (@table) {
-    $body .= "<tr><td>$reg</td></tr>\n";
-  }
-  return $body."</table>\n";
-}
