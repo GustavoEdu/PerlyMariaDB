7b11c44b221f8e2cf62dcef1d55c01369bcb98f9
Gustavo <gordono@unsa.edu.pe>
2021-11-30 04:59:26 +0000

    Ejercicio4 (Extendido): Se ha modificado la Subrutina renderTable para que procese toda la Información que le mandamos. En primera instancia, realiza una asignación de campos hacia los arreglos vacíos, se Empareja automáticamente después de esto y se manda a construir el Formato Tabla, después se reutiliza la Subrutina renderBody y se pasan como argumentos junto al Titulo de la Página Web para mostrar como Resultado de la Consulta

--- queryByYear.pl
+++ queryByYear.pl
@@ -42,9 +42,10 @@ $dbh->disconnect;
 
 print $q->header('text/html;charset=UTF-8');
 
-#my $body = renderTable($year, @resp);
+my $table = renderTable(@titles, @years, @scores, @votes);
+my $body = renderBody("Resultados de la Consulta", $table);
 
-#print renderHTMLpage("Películas de $year", 'css/mystyle.css', $body);
+print renderHTMLpage("Películas de $year", 'css/mystyle.css', $body);
 
 sub renderHTMLpage {
   my $title = $_[0];
@@ -68,14 +69,54 @@ HTML
 }
 
 sub renderTable {
-  my $year = $_[0];
-  my @table  = @_;
-  shift(@table);
+  my @data = @_;
+  my @titles;
+  my @years;
+  my @scores;
+  my @votes;
 
-  my $body = "<h1 class='titulo'>Películas de $year</h1>\n<table>\n";
-  $body .= "<tr><th>Titles</th></tr>\n";
-  foreach my $reg (@table) {
-    $body .= "<tr><td>$reg</td></tr>\n";
+  #Repartimos @data en @titles, @years, @scores y @votes
+  my $longitud = scalar(@data)/4;
+  for(my $i = 0; $i < $longitud; $i++) {
+    push(@titles, shift(@data));
   }
-  return $body."</table>\n";
+  for(my $i = 0; $i < $longitud; $i++) {
+    push(@years, shift(@data));
+  }
+  for(my $i = 0; $i < $longitud; $i++) {
+    push(@scores, shift(@data));
+  }
+  for(my $i = 0; $i < $longitud; $i++) {
+    push(@votes, shift(@data));
+  }
+
+  #Se comienza a construir la Tabla
+  my $table = "<table>\n";
+  $table .= "<tr><th>Películas Encontradas</th>\n";
+  $table .= "<th>Year</th>\n";
+  $table .= "<th>Score</th>\n";
+  $table .= "<th>Votes</th></tr>\n";
+
+  for(my $i = 0; $i < $longitud; $i++) {
+    $table .= "<tr><td>$titles[$i]</td>\n";
+    $table .= "<td>$years[$i]</td>\n";
+    $table .= "<td>$scores[$i]</td>\n";
+    $table .= "<td>$votes[$i]</td></tr>\n";
+  }
+
+  return $table."</table>\n";
 }
+
+sub renderBody {
+  my $titulo = $_[0];
+  my $table = $_[1];
+
+  my $body = <<"BODY";
+  <h1 class='titulo'>$titulo</h1>
+$table
+  <br>
+  Ingrese <a href="Ejercicio4.pl">aquí</a> para realizar otra consulta.
+BODY
+  return $body;
+}
+
